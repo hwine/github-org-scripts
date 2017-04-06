e3e0b581ba8678ca72fa657ed6db027949d2b7c0
Hal Wine <hwine@mozilla.com>
2017-04-06 15:07:24 -0700

    Work with github3.py > 1.0

--- license.py
+++ license.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
-import sys
 
 import requests
 
 from client import get_github3_client
+
 
 def report_repos(repo_list, msg):
     print "Found %d repos with %s." % (len(repo_list), msg)
@@ -14,27 +14,30 @@ if __name__ == '__main__':
     gh = get_github3_client()
     headers = {
         'Accept': 'application/vnd.github.drax-preview+json',
-        #'Authorization': 'token %s' % get_token()
     }
 
     # Divvy up into repositories that have/do not have a LICENSE file.
     mpl_repos = []
     good_repos = []
     bad_repos = []
+    gpl_repos = []
 
     org_name = 'mozilla'
-    repos = gh.organization(org_name).iter_repos(type='sources')
+    repos = gh.organization(org_name).repositories(type='sources')
     for repo in repos:
         repo_name = repo.name
         license_url = 'https://api.github.com/repos/%s/%s' % (org_name,
-                repo_name)
+                                                              repo_name)
 
         resp = requests.get(license_url, headers=headers)
         repo_data = resp.json()
 
         try:
-            if repo_data[u'license'][u'key'] == 'mpl-2.0':
+            license_type = repo_data[u'license'][u'key']
+            if license_type == 'mpl-2.0':
                 mpl_repos.append(repo.full_name)
+            elif 'gpl' in license_type:
+                gpl_repos.append(repo.full_name)
             else:
                 good_repos.append(repo.full_name)
         except (KeyError, TypeError):
@@ -43,8 +46,7 @@ if __name__ == '__main__':
             # github understands)
             bad_repos.append(repo.full_name)
 
-
-
     report_repos(mpl_repos, "an MPL license")
+    report_repos(gpl_repos, "some sort of GPL license")
     report_repos(good_repos, "a license")
     report_repos(bad_repos, "NO License")
